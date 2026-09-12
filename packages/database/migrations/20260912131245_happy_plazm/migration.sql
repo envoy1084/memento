0817e4e1ef57156935f5a96caa9fe55d462c9f3c
Vedant Chainani <vedantchainani1084@gmail.com>
2026-09-13 03:27:28 +0530

    chore(database): remove superseded development migration

--- packages/database/migrations/20260912131245_happy_plazm/migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "ens_workflows" (
-	"namespace" text,
-	"id" text,
-	"revision" integer NOT NULL,
-	"valueCiphertext" text NOT NULL,
-	CONSTRAINT "ens_workflows_pkey" PRIMARY KEY("namespace","id"),
-	CONSTRAINT "ens_workflow_revision" CHECK ("revision" >= 0)
-);
